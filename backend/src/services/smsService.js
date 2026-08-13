57ac8cf4ac925cb239b5a6932cbaf02f239312f6
MIhirDas10 <dasmihir2911@gmail.com>
2026-08-14 02:55:42 +0600

    Cleaned up old folders and moved merged code to root

--- backend/src/services/smsService.js
+++
@@ -1,5 +0,0 @@
-exports.sendStatusUpdateSMS = async (phoneNumber, status) => {
-    if (!phoneNumber) return;
-    console.log(`Sending SMS to ${phoneNumber}: Your invoice status has advanced to ${status}`);
-
-};
